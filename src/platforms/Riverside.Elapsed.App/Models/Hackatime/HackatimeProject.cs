2e328446828c37e0c346f76fecf03e52b8a1d1ae
Lamparter <71598437+Lamparter@users.noreply.github.com>
2026-05-04 10:14:38 +0100

    Add `HackatimeProject` and `HackatimeProjectTimelapses` models

---
+++ src/platforms/Riverside.Elapsed.App/Models/Hackatime/HackatimeProject.cs
@@ -0,0 +1,7 @@
+namespace Riverside.Elapsed.App.Models.Hackatime;
+
+public sealed class HackatimeProject
+{
+	public string Name;
+	public double TotalSeconds;
+}
---
+++ src/platforms/Riverside.Elapsed.App/Models/Hackatime/HackatimeProjectTimelapses.cs
@@ -0,0 +1,7 @@
+namespace Riverside.Elapsed.App.Models.Hackatime;
+
+public sealed class HackatimeProjectTimelapses
+{
+	public double Count;
+	public IReadOnlyList<Timelapses.Timelapse> Timelapses;
+}
