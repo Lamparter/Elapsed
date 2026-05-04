c1965842b785d428dc3e046f4a8f3bfa1fbe66fd
Lamparter <71598437+Lamparter@users.noreply.github.com>
2026-05-04 10:13:57 +0100

    Add `ActiveUser` and `LeaderboardEntry` models

---
+++ src/platforms/Riverside.Elapsed.App/Models/Global/ActiveUsers.cs
@@ -0,0 +1,6 @@
+namespace Riverside.Elapsed.App.Models.Global;
+
+public sealed class ActiveUsers
+{
+	public double Count;
+}
---
+++ src/platforms/Riverside.Elapsed.App/Models/Global/LeaderboardEntry.cs
@@ -0,0 +1,7 @@
+namespace Riverside.Elapsed.App.Models.Global;
+
+public sealed class LeaderboardEntry
+{
+	public User.User User;
+	public double SecondsThisWeek;
+}
